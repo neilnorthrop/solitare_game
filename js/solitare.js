53a28c8a10f0982fa65346e7d8246b756c33f77b
keighty <k80.leonard@gmail.com>
2013-06-19 17:31:57 -0700

    added restrictions to square creation and class matching

--- js/solitare.js
+++ js/solitare.js
@@ -2,16 +2,33 @@ $(function(){
 	var new_square = '<div class="square"></div>';
 
 	$('body').on('click','.square', function(){
-		// var my_square = $(this);
-		$(this).toggleClass('blue');
+		var my_square = $(this),
+				my_square_classes;
 
-		b = $(new_square)
-			.insertAfter(this);
-		a = $(new_square)
-			.insertBefore(this);
-		b.toggleClass('blue');
-		a.toggleClass('blue');
+		my_square.toggleClass('blue');
+		my_square_classes = my_square.attr("class");
+
+		if (my_square.next().is('div')){
+			match_classes(my_square.next(), my_square_classes);
+		} else {
+			var next_sq = $(new_square).insertAfter(this);
+			match_classes(next_sq, my_square_classes);
+		}
+
+		if (my_square.prev().is('div')){
+			match_classes(my_square.prev(), my_square_classes);
+		} else {
+			var prev_sq = $(new_square).insertBefore(this);
+			match_classes(prev_sq, my_square_classes);
+		}
+
 	});
+
+	var match_classes = function(square, classes) {
+		square
+			.removeClass()
+			.attr("class", classes);
+	};
 
 
 });
